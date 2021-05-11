8b6916fd335852f6aeab1793ebd90fab59bdb0e0
Iryna Konoplova <a.irina773@gmail.com>
2021-05-11 17:09:08 +0300

    Решает задачу

--- color-switch.js
+++ color-switch.js
@@ -7,23 +7,32 @@ const colors = [
   '#795548',
 ];
 
+const min = 0;
+const max = colors.length-1;
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+let intervalId;
 
 const buttonStart = document.querySelector("[data-action='start']");
 const buttonStop = document.querySelector("[data-action='stop']");
 const bodyEl = document.querySelector("body");
 
 buttonStart.addEventListener('click', onButtonStartClick)
-// buttonStop.addEventListener('click', onButtonStopClick)
-
-
-
-
-
+buttonStop.addEventListener('click', onButtonStopClick)
 
 function onButtonStartClick(e) {
-    bodyEl.style.backgroundColor = "red";
-   console.log(bodyEl)
+   intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = colors[randomIntegerFromInterval(min, max)];
+  }, 1000);
+  
+  buttonStart.setAttribute('disabled', false);
+  return intervalId;
+}
+
+function onButtonStopClick(e) {
+  buttonStart.removeAttribute('disabled');
+  clearInterval(intervalId);
 }
